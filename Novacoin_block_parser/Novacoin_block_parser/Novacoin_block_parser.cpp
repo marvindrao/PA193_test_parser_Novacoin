7e760f5abb343e207f39d34124fff27b89873192
arvind <arvindrao7589@gmail.com>
2017-09-25 22:54:10 +0200

    Added .gitignore to the root of repo, comments in code and ifdef macros

--- Novacoin_block_parser/Novacoin_block_parser/Novacoin_block_parser.cpp
+++ Novacoin_block_parser/Novacoin_block_parser/Novacoin_block_parser.cpp
@@ -11,7 +11,7 @@ int main()
 {
 	ifstream block;
 	cout << "NovaCoin Block Parser"<<endl;
-	block.open("block2", ios::binary);
+	block.open("block1", ios::binary);
 	if (!block.is_open())
 	{
 		cout << "file not open" << endl;
@@ -19,12 +19,16 @@ int main()
 	}
 	uint32_t magic= 0;
 	uint32_t bsize = 0;
-
+	/* gist.github.com/molpopgen/9123133   Illustrates some of the various quirks/annoyances to read/write binary stuff in C++ */
 	block.read(reinterpret_cast<char *>(&magic), sizeof(magic));
-	cout << hex << magic<<endl;
+#ifdef _DEBUG
+	cout << "Magic Number read:"<< hex << magic << endl;
+#endif // DEBUG
 
 	block.read(reinterpret_cast<char *>(&bsize), sizeof(bsize));
-	cout << dec<<bsize << endl;
+#ifdef _DEBUG
+	cout <<"File Size read:"<< dec << bsize << endl;
+#endif // DEBUG
 	if (magic != 0xe5e9e8e4)
 	{
 		cout << "Incorrect Magic in header" << endl;
@@ -33,12 +37,14 @@ int main()
 	uint32_t beg = 0,bfsize=0;
 	beg = block.tellg();
 	block.seekg(0,block.end);
-	bfsize = uint32_t(block.tellg() ) -  beg;
-	cout << bfsize<<endl;
+	bfsize = uint32_t(block.tellg() ) -  beg ;
+#ifdef _DEBUG
+	cout <<"File Size Calculated:"<< bfsize<<endl;
+#endif
 	if (bfsize != bsize)
 	{
 		cout << "Incorrect Size in header" << endl;
-		exit(-100);
+		exit(-101);
 	}
 	block.seekg(beg);
 
