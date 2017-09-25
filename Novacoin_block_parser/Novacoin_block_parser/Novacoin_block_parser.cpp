f67b16cf18b1ac13fabf7039776dfd81970d68b9
arvind <arvindrao7589@gmail.com>
2017-09-25 13:20:57 +0200

    After creating blank VS2015 project.

---
+++ Novacoin_block_parser/Novacoin_block_parser/Novacoin_block_parser.cpp
@@ -0,0 +1,12 @@
+// Novacoin_block_parser.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "NovaCoin Block Parser"<<std::endl;
+}
+
